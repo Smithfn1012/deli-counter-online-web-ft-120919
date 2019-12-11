325e80f3cc60b29ddd64d438425ac653e0c3bc98
Felicia Smith <smithfn1012@gmail.com>
2019-12-11 17:42:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(array)
   if array.size == 0 
-    puts "This line is currently empty."
+    puts "The line is currently empty."
   else
     message = "The line is currently: "
     array.each_with_index do |name, index| 
