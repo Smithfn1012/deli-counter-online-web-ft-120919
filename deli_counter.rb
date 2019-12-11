1cdff5516ce4d3ab7a5b1cae16838856b9eae8bb
Felicia Smith <smithfn1012@gmail.com>
2019-12-11 17:41:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,18 @@ def line(array)
   end
 end
 
+def take_a_number(line_array, name)
+  line_array << name
+  puts "Welcome, #{name}. You are number #{line_array.size} in line."
+end
 
+def now_serving(line_array)
+  if line_array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_customer = line_array.shift
+    puts "Currently serving #{current_customer}."
+  end
+end
   
   
